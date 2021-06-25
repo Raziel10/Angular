8b09562ff9a474f8cfa44c1fef1739feabf94449
Lopez <razielescamilla@outlook.com>
2021-06-25 12:31:32 -0500

    RxJs Part 1

--- conFusion/src/app/services/leader.service.ts
+++ conFusion/src/app/services/leader.service.ts
@@ -11,11 +11,11 @@ export class LeaderService {
 
   constructor() { }
 
-  getLeaders(): Promise<Leader[]>{
-    return of(LEADERS).pipe(delay(2000)).toPromise();
+  getLeaders(): Observable<Leader[]>{
+    return of(LEADERS).pipe(delay(2000));
   }
 
-  getFeaturedLeader(): Promise<Leader>{
-    return of(LEADERS.filter((leader) => (leader.featured))[0]).toPromise();
+  getFeaturedLeader(): Observable<Leader>{
+    return of(LEADERS.filter((leader) => (leader.featured))[0]).pipe(delay(2000));
   }
 }
--- conFusion/src/app/services/promotion.service.ts
+++ conFusion/src/app/services/promotion.service.ts
@@ -11,15 +11,15 @@ export class PromotionService {
 
   constructor() { }
 
-  getPromotions(): Promise<Promotion[]> {
-    return of(PROMOTIONS).pipe(delay(2000)).toPromise();
+  getPromotions(): Observable<Promotion[]> {
+    return of(PROMOTIONS).pipe(delay(2000));
   }
 
-  getPromotion(id: string): Promise<Promotion> {
-    return of(PROMOTIONS.filter((promo) => (promo.id === id))[0]).pipe(delay(2000)).toPromise();
+  getPromotion(id: string): Observable<Promotion> {
+    return of(PROMOTIONS.filter((promo) => (promo.id === id))[0]).pipe(delay(2000));
   }
 
-  getFeaturedPromotion(): Promise<Promotion> {
-    return of(PROMOTIONS.filter((promotion) => promotion.featured)[0]).pipe(delay(2000)).toPromise();
+  getFeaturedPromotion(): Observable<Promotion> {
+    return of(PROMOTIONS.filter((promotion) => promotion.featured)[0]).pipe(delay(2000));
   }
 }
